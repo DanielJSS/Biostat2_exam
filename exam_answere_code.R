f86d673b3c5dc06cc1496c551d00d3003724524c
DanielJSS <danieljss98@gmail.com>
2023-06-13 21:37:12 +0200

    First Push

--- exam_answere_code.R
+++ exam_answere_code.R
@@ -5,8 +5,5 @@ use_git_config(
   user.name = "DanielJSS", 
   user.email = "danieljss98@gmail.com"
 )
-gitcreds::gitcreds_set()
+0
 3
-git remote add origin https://github.com/DanielJSS/Biostat2_exam.git
-git branch -M main
-git push -u origin main
